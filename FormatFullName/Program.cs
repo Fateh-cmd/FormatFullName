aae633013d02811465e841661d38c432b3e82ed8
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-09-22 23:38:19 +0200

    Add FormatFullName files

---
+++ FormatFullName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static string FormatFullName(string firstName, string lastName)
+    {
+        string formattedName = $"{lastName}, {firstName}";
+        return formattedName;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        string formattedFullName = FormatFullName(firstName, lastName);
+        Console.WriteLine($"Formatted Name: {formattedFullName}");
+    }
+}
+
